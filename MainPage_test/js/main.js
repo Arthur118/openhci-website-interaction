fe04af81e4ec8012c8b383e193c034ac90a92e68
110619045 <0304phoebe@gmail.com>
2022-07-11 00:03:01 +0800

    0304phoebe
    Schedule carousel

--- MainPage_test/js/main.js
+++ MainPage_test/js/main.js
@@ -1,17 +1,3 @@
-// function click_card_open() {
-//     document.getElementById('card_a_content').style.display='block';
-//     document.getElementById('card_a_btn_close').style.display='block';
-//     document.getElementById('card_a_btn_open').style.display='none';
-//     document.getElementById('card_a_title').style.color='#56FAFB';
-// }
-
-// function click_card_close() {
-//     document.getElementById('card_a_content').style.display='none';
-//     document.getElementById('card_a_btn_close').style.display='none';
-//     document.getElementById('card_a_btn_open').style.display='block';
-//     document.getElementById('card_a_title').style.color='#fff';
-// }
-
 $(function() {
     $(".panel-collapse").on('show.bs.collapse', function() {
         $(this).siblings('.card_a_close').addClass('active');
@@ -34,24 +20,6 @@ $(function() {
     });
     
 });
-
-$('.panel-collapse').on('hidden.bs.collapse', function () {
-    console.log("open");
-});
-
-$('.panel-collapse').on('show.bs.collapse', function () {
-    console.log("close");
-})
-
-// $('.panel-collapse').on('show.bs.collapse', function () {
-//     $(this).siblings('.card_a_close').addClass('active');
-//     console.log("open");
-// });
-
-// $('.panel-collapse').on('hide.bs.collapse', function () {
-//     $(this).siblings('.card_a_close').removeClass('active');
-//     console.log("hide");
-// });
 
 const scheduleInfo = {
     "前置工作坊": [
