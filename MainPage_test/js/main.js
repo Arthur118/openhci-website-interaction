d15c2f8dbd28881ec90a67af5e7c96f534651f70
RabbitMeat <aliceapplehappy@gmail.com>
2022-07-18 07:08:48 +0800

    0708

--- MainPage_test/js/main.js
+++ MainPage_test/js/main.js
@@ -1,5 +1,10 @@
 import { main } from "./tower.js"
 
+// First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+let vh = window.innerHeight * 0.01;
+// Then we set the value in the --vh custom property to the root of the document
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+
 $(function() {
 // model position control
     let windowSize = window.matchMedia("(min-width: 768px)")
@@ -83,7 +88,7 @@ $(function() {
 
         for (var i = 14; i <= 15; i++) {
             if(scrollPos >= ah[i-1] - windowHeight/1.5){
-                console.log('show');
+                //console.log('show');
                 $('section:nth-of-type('+i+') logo').addClass('fade_in_logo');
             }
         }
